662df91dc792d1b90e4d6270894bc30ebe790cc7
Gregory Arthus <gr.arthus@gmail.com>
2024-02-11 22:24:28 +0100

    direct cam pic analyse

--- variables.py
+++ variables.py
@@ -2,13 +2,3 @@
 captdir=''
 # File to trigger an event if human is detected
 trigger=''
-# remote ssh port
-sshport=''
-# remote ssh key
-sshkey=''
-# user@address of remote server
-sshserv=''
-# folder of human detected
-detectdir=''
-# folder of not human detected
-nonedir=''
